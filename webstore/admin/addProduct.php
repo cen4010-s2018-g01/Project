bcb4d9dcd19ca73848efab7181dbba3bd41bc1c2
DBerry <dwjberry07@gmail.com>
2018-04-25 17:58:09 -0400

    Small fix to add product to listing

--- webstore/admin/addProduct.php
+++ webstore/admin/addProduct.php
@@ -15,7 +15,7 @@ $price = $_POST["price"];
 $db = "cen4010_s2018g01";
 $table = "inventory";
 
-if ($quant >= 0 || price >= 0){
+if ($quant <= 0 || $price <= 0){
     $_SESSION["phrase"] = "ERROR <br> Invalid inputs in New Product <br> quantity and/or price fields";
     header('Location: adminForm.php');
     return;    
